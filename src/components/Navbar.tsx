2b49bac7ffd0eb056ce0823365fc34a75f016e76
Jonathan Bracho <jonajes0288@gmail.com>
2023-03-16 19:14:03 +0100

    [code]: Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+import { NavItem } from './';
+import { navItems } from '../data';
+
+export const Navbar: React.FC = () => {
+  //
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <nav className='font-body lg:hidden'>
+        <button
+          className={`flex items-center px-3 py-2 border rounded text-text border-text hover:text-primary hover:border-primary ${
+            isOpen ? 'hidden' : 'block'
+          }`}
+          onClick={toggleMenu}
+        >
+          <svg className='w-3 h-3 fill-current' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+            <title>Menu</title>
+            <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+          </svg>
+        </button>
+      </nav>
+      <nav className={`font-body ${isOpen ? 'block' : 'hidden'} lg:flex lg:items-center`}>
+        <ul className='flex flex-col lg:flex-row lg:space-x-6'>
+          {navItems.map(({ label, path }) => (
+            <NavItem key={path} label={label} path={path} toggleMenu={toggleMenu} />
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
+};
