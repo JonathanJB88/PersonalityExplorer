f8f4cea7a138d92b9832ee478abc1e1b50a634fc
Jonathan Bracho <jonajes0288@gmail.com>
2023-03-16 21:42:43 +0100

    [Add]: CTASection component

---
+++ src/components/CTASection.tsx
@@ -0,0 +1,12 @@
+import { Btn } from './Btn';
+
+export const CTASection: React.FC = () => {
+  return (
+    <section className='py-12 bg-background text-text'>
+      <div className='container px-4 mx-auto text-center'>
+        <h2 className='text-2xl font-bold font-header md:text-4xl'>Are you ready to explore your personality?</h2>
+        <Btn label='Start the test now' />
+      </div>
+    </section>
+  );
+};
