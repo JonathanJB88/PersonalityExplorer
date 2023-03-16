490f8f011d6a77ba48b8748aad5df6d7be2ccc72
Jonathan Bracho <jonajes0288@gmail.com>
2023-03-16 21:43:14 +0100

    [Add]: FeaturesSection component

---
+++ src/components/FeaturesSection.tsx
@@ -0,0 +1,16 @@
+import { features } from '../data';
+import { FeatureCard } from './';
+
+export const FeaturesSection: React.FC = () => {
+  return (
+    <section className='py-8 bg-transparent'>
+      <div className='container px-4 mx-auto'>
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
+          {features.map(({ id, title, description }) => (
+            <FeatureCard key={id} title={title} description={description} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
